d9ed9d8ea13c9a020cdff15fd1bdae6b5be87db4
Matt Williams <matt@milliams.com>
2020-10-21 15:16:22 +0100

    First steps of generic installer

---
+++ install-citc.py
@@ -0,0 +1,36 @@
+from __future__ import print_function
+
+import os
+import stat
+import sys
+from subprocess import check_call
+try:
+    from urllib.request import urlretrieve
+except ImportError:
+    from urllib import urlretrieve
+from zipfile import ZipFile
+
+def main():
+    tf_repo_zip, _ = urlretrieve("https://github.com/clusterinthecloud/terraform/archive/master.zip")
+    ZipFile(tf_repo_zip).extractall()
+
+    if sys.platform.startswith("linux"):
+        tf_platform = "linux_amd64"
+    elif sys.platform == "darwin":
+        tf_platform = "darwin_amd64"
+    elif sys.platform == "win32":
+        raise NotImplementedError("Windows is not supported at the mooment")
+    else:
+        raise NotImplementedError("Platform is not supported")
+
+    tf_version = "0.12.29"
+    tf_template = "https://releases.hashicorp.com/terraform/{v}/terraform_{v}_{p}.zip"
+    tf_url = tf_template.format(v=tf_version, p=tf_platform)
+    tf_zip, _ = urlretrieve(tf_url)
+    ZipFile(tf_zip).extractall()
+    os.chmod("terraform", stat.S_IRWXU)
+
+    check_call(["./terraform", "init", "terraform-master/aws"])
+
+if __name__ == "__main__":
+    main()
---
+++ install-citc.sh
@@ -0,0 +1,6 @@
+#! /bin/sh
+set -eu
+
+PYTHON="$(command -v python3 || command -v python)"
+DIR=$(CDPATH= cd -- "$(dirname -- "$0")" && pwd -P)
+"${PYTHON}" "${DIR}/install-citc.py"
