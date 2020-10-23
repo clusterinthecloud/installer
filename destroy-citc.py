b45eb5cb017503ee95ed64cdf1a03e8504aca462
Matt Williams <matt@milliams.com>
2020-10-23 16:01:09 +0100

    Name the tf dir after the cluster ID

--- destroy-citc.py
+++ destroy-citc.py
@@ -21,22 +21,20 @@ def main():
     parser.add_argument("--dry-run", help="Perform a dry run", action="store_true")
     args = parser.parse_args()
 
-    tf_zip = "citc-terraform.zip"
-    check_call(["scp", "-i", args.key, "citc@{}:{}".format(args.ip, tf_zip), "."])
-    ZipFile(tf_zip).extractall("downloaded-citc-terraform")
-
-    # Shut down any running packer builders
-    if not args.dry_run:
-        pass#check_call(["ssh", "-i", args.key, "citc@{}".format(args.ip), "/usr/local/bin/kill_packer"])i
+    tf_zip_filename = "citc-terraform.zip"
+    check_call(["scp", "-i", args.key, "citc@{}:{}".format(args.ip, tf_zip_filename), "."])
+    tf_zip = ZipFile(tf_zip_filename)
+    dir_name = tf_zip.namelist()[0]
+    tf_zip.extractall()
 
     # Shut down any running compute nodes and delete associated DNS entries
     if not args.dry_run:
         try:
             check_call(["ssh", "-i", args.key, "citc@{}".format(args.ip), "/usr/local/bin/kill_all_nodes"])
         except CalledProcessError:
-            print("/usr/local/bin/kill_all_nodes failed to run. You may have lingering compute nodes. You must killi these manually.")
+            print("/usr/local/bin/kill_all_nodes failed to run. You may have lingering compute nodes. You must kill these manually.")
 
-    os.chdir("downloaded-citc-terraform")
+    os.chdir(dir_name)
 
     check_call(["./terraform", "init", args.csp])
 
@@ -45,7 +43,7 @@ def main():
             check_call(["./terraform", "destroy", "-auto-approve", args.csp])
         except CalledProcessError:
             print("Terraform destroy failed. Try again with:")
-            print("  cd downloaded-citc-terraform")
+            print("  cd {}".format(dir_name))
             print("  ./terraform destroy {}".format(args.csp))
             print("You may need to manually clean up any remaining running instances or DNS entries")
 
--- install-citc.py
+++ install-citc.py
@@ -21,12 +21,14 @@ def main():
     parser.add_argument("--availability_zone", help="AWS availability zone")
     args = parser.parse_args()
 
+    #Download the CitC Terraform repo
     tf_repo_zip, _ = urlretrieve("https://github.com/clusterinthecloud/terraform/archive/master.zip")
     ZipFile(tf_repo_zip).extractall()
     shutil.rmtree("citc-terraform", ignore_errors=True)
     os.rename("terraform-master", "citc-terraform")
     os.chdir("citc-terraform")
 
+    # Download Terraform binary
     if sys.platform.startswith("linux"):
         tf_platform = "linux_amd64"
     elif sys.platform == "darwin":
@@ -35,7 +37,6 @@ def main():
         raise NotImplementedError("Windows is not supported at the mooment")
     else:
         raise NotImplementedError("Platform is not supported")
-
     tf_version = "0.12.29"
     tf_template = "https://releases.hashicorp.com/terraform/{v}/terraform_{v}_{p}.zip"
     tf_url = tf_template.format(v=tf_version, p=tf_platform)
@@ -43,34 +44,45 @@ def main():
     ZipFile(tf_zip).extractall()
     os.chmod("terraform", stat.S_IRWXU)
 
+    # Create key for admin and provisioning
     if not os.path.isfile("citc-key"):
         check_call(["ssh-keygen", "-t", "rsa", "-f", "citc-key", "-N", ""])
 
+    # Intialise Terraform
     check_call(["./terraform", "init", args.csp])
     check_call(["./terraform", "validate", args.csp])
 
+    # Set up the variable file
     config_file(args.csp, args)
 
+    # Create the cluster
     if not args.dry_run:
         check_call(["./terraform", "apply", "-auto-approve", args.csp])
 
+    # Get the outputs
     ip = check_output(["./terraform", "output", "-no-color", "-state=terraform.tfstate", "ManagementPublicIP"]).decode().strip()
+    cluster_id = check_output(["./terraform", "output", "-no-color", "-state=terraform.tfstate", "cluster_id"]).decode().strip()
 
     os.chdir("..")
+    new_dir_name = "citc-terraform-{}".format(cluster_id)
+    os.rename("citc-terraform", new_dir_name)
 
-    tf_zip = shutil.make_archive("citc-terraform", "zip", "citc-terraform")
+    tf_zip = shutil.make_archive("citc-terraform", "zip", ".", new_dir_name)
     if not args.dry_run:
         check_call(["scp", "-i", "citc-terraform/citc-key", tf_zip, "citc@{}:.".format(ip)])
+    os.remove(tf_zip)
+
+    key_path = "{}/citc-key".format(new_dir_name)
 
     print("")
     print("#"*80)
     print("")
-    print("The file 'citc-terraform/citc-key' will allow you to log into the new cluster")
+    print("The file '{}' will allow you to log into the new cluster".format(key_path))
     print("Make sure you save this key as it is needed to destroy the cluster later.")
 
     print("The IP address of the cluster is {}".format(ip))
     print("Connect with:")
-    print("  ssh -i {ssh_id} citc@{ip}".format(ssh_id="citc-terraform/citc-key", ip=ip))
+    print("  ssh -i {ssh_id} citc@{ip}".format(ssh_id=key_path, ip=ip))
 
 
 def config_file(csp, args):
