14badb12d2b1d47c525c80d4726b71281a780f68
Matt Williams <matt@milliams.com>
2021-08-11 15:54:14 +0100

    Let the Python script do the argument checking

--- install-citc.sh
+++ install-citc.sh
@@ -1,11 +1,6 @@
 #! /bin/sh
 set -eu
 
-if [ $# -ne 1 ]; then
-    echo "ERROR: Usage: ${0} <cloud_provider>" >&2
-    exit 1
-fi
-
 PYTHON="$(command -v python3 || command -v python)"
 DIR=$(CDPATH='' cd -- "$(dirname -- "$0")" && pwd -P)
 "${PYTHON}" "${DIR}/install-citc.py" "${@}"
